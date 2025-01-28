da811ccca1ad7be56e8fbea7ea878f3ee84a3f2f
FaaizAhmad6420 <faizahmad6420@gmail.com>
2025-01-28 15:41:53 +0500

    Counter

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const increaseCount = () => {
+    setCount(count + 1)
+  }
+
+  const decreaseCount = () => {
+    setCount(count + 1)
+  }
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <h3>{count}</h3>
+      <button onClick={increaseCount} >+</button>
+      <button onClick={decreaseCount} >-</button>
+    </>
+  )
+}
+
+export default App
